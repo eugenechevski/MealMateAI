f4f25c18d69ff99738f71c22f9210166c86e06a2
Eugene Chevski <eugenechevski@proton.me>
2024-03-06 23:17:31 -0500

    adds favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Meal Mate AI",
   description: "Meal Mate AI is a meal planning app that uses AI to help you plan your meals.",
+  icons: {
+    icon: "@/app/assets/favicon.ico",
+  }
 };
 
 export default function RootLayout({
