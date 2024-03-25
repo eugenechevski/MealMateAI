60252197b176e3b8d16e3d15c59393d04d011b9c
Eugene Chevski <eugenechevski@proton.me>
2024-03-25 12:05:32 -0400

    adds animation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
+"use client";
+
 import type { Metadata } from "next";
 import { Pacifico, Roboto_Serif } from "next/font/google";
 import Image from "next/image";
 import logoImg from "@/assets/logo.png";
+import { motion } from "framer-motion";
 import "./globals.css";
-
 
 const primaryFont = Roboto_Serif({
   subsets: ["latin"],
@@ -19,12 +21,12 @@ const secondaryFont = Pacifico({
   weight: "400",
 });
 
-export const metadata: Metadata = {
+const metadata = {
   title: "Meal Mate AI",
   description:
     "Meal Mate AI is a meal planning app that uses AI to help you plan your meals.",
   icons: {
-    icon: "@/app/assets/logo.ico",
+    icon: "@/app/favicon.ico",
   },
 };
 
@@ -38,12 +40,25 @@ export default function RootLayout({
       lang="en"
       className={`${primaryFont.variable} ${secondaryFont.variable}`}
     >
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content={metadata.description} />
+        <link rel="icon" href={metadata.icons.icon} type="image/x-icon" />
+        <title>{metadata.title}</title>
+      </head>
       <body className="bg-background-accent relative text-primary-text text-shadow scroll-smooth hide-scrollbar snap-center snap-normal snap-mandatory max-h-max max-w-max">
         {/* Floating logo */}
-        <div className="absolute left-10 top-10 w-32 flex flex-col items-center justify-center">
-          <Image src={logoImg} alt="Meal Mate AI logo"/>
+        <motion.figure
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2 }}
+          whileHover={{ scale: 1.1 }}
+          className="absolute left-10 top-10 w-32 flex flex-col items-center justify-center"
+        >
+          <Image src={logoImg} alt="Meal Mate AI logo" />
           <span className="font-secondary select-none">Meal Mate AI</span>
-        </div>
+        </motion.figure>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,34 @@
-import { createClient } from "@/lib/supabase/server";
-import { cookies } from "next/headers";
+"use client";
+
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import landingPageImg from "@/assets/landing-page.png";
 import Image from "next/image";
 import Button from "@/components/Button";
+import { motion } from "framer-motion";
 
-export default async function Page() {
-  const cookieStore = cookies();
-  const supabase = createClient(cookieStore);
-
+export default function Page() {
   return (
-    <main className="w-screen h-screen flex flex-col justify-center items-center gap-3">
-      <h1 className="text-3xl font-secondary">Simplify Your Meal Planning</h1>
-      <figure className="rounded-full shadow-2xl">
+    <motion.main
+      className="w-screen h-screen flex flex-col justify-center items-center gap-3"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+    >
+      <motion.h1
+        className="text-3xl font-secondary"
+        initial={{ y: -50, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 1 }}
+      >
+        Simplify Your Meal Planning
+      </motion.h1>
+      <motion.figure
+        className="rounded-full shadow-2xl"
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        transition={{ duration: 1 }}
+      >
         <Image
           src={landingPageImg}
           width={300}
@@ -21,13 +36,26 @@ export default async function Page() {
           alt="Landing Page"
           className="rounded-full shadow-2xl"
         ></Image>
-      </figure>
-      <p className="font-primary italic">
+      </motion.figure>
+      <motion.p
+        className="font-primary italic"
+        initial={{ y: 50, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 1 }}
+      >
         Deciding what to eat has never been easier.
-      </p>
-      <Button>
-        <FontAwesomeIcon icon={faArrowRight} className="w-6" size="3x"/>
-      </Button>
-    </main>
+      </motion.p>
+      <motion.div
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        transition={{ duration: 1 }}
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+      >
+        <Button size="md">
+          <FontAwesomeIcon icon={faArrowRight} className="w-12" size="2x" />
+        </Button>
+      </motion.div>
+    </motion.main>
   );
 }
